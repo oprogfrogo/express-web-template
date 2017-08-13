9e3e27115cd4a33be94b5fbf61f9fe91bd5b4f21
Simon Lee <oprogfrogo@gmail.com>
2017-08-13 14:54:30 -0700

    switch it up to bootstrap.

--- gulpfile.js
+++ gulpfile.js
@@ -20,34 +20,31 @@ gulp.task('bower', () => {
 gulp.task('css:application', ['bower'], () => {
 
     return gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.min.css',
         'bower_components/datatables/media/css/jquery.dataTables.min.css',
-        'bower_components/datatables.net-zf/css/dataTables.foundation.min.css',
         'bower_components/flatpickr/dist/flatpickr.min.css',
         'bower_components/font-awesome/css/font-awesome.min.css',
-        'bower_components/foundation-sites/dist/foundation.min.css',
-        'server/public/src/stylesheets/application.css'
+        'server/public/src/css/application.css'
     ])
     .pipe(concat('application.css'))
     .pipe(cssmin())
-    .pipe(gulp.dest('server/public/dist/stylesheets'));
+    .pipe(gulp.dest('server/public/dist/css'));
 });
 
 gulp.task('js:application', ['bower'], () => {
 
     return gulp.src([
         'bower_components/jquery/dist/jquery.js',
-        'bower_components/motion-ui/dist/motion-ui.js',
-        'bower_components/foundation-sites/dist/foundation.js',
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
         'bower_components/datatables.net/js/jquery.dataTables.min.js',
-        'bower_components/datatables.net-zf/js/dataTables.foundation.min.js',
         'bower_components/flatpickr/dist/flatpickr.min.js',
-        'server/public/src/javascripts/application.js'
+        'server/public/src/js/application.js'
     ])
     .pipe(concat('application.js'))
     .pipe(uglify().on('error', function(e){
             console.log(e);
          }))
-    .pipe(gulp.dest('server/public/dist/javascripts'));
+    .pipe(gulp.dest('server/public/dist/js'));
 });
 
 gulp.task('images:default', ['bower'], () => {
